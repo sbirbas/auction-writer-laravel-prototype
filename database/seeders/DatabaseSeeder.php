723bad0e04abce6cbd38bf65f23af1f6db9349c2
Sophia Birbas <sophiabirbas@Sophias-MacBook-Air.local>
2025-04-04 19:59:39 -0400

    fix seeder to seed a user first before seeding listings

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\Listing;
+use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Listing::factory(10)->create();
+        $user = User::create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        Listing::factory(10)->create([
+            'user_id' => $user->id
+        ]);
     }
 }
